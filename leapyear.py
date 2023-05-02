a736210ab816972b610554ef1cbcdafea87b7d8f
konkeul <yrose8556@gmail.com>
2023-05-02 18:12:33 +0900

    Hello, I'm 0won

--- leapyear.py
+++ leapyear.py
@@ -4,3 +4,11 @@ if ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 ==0) :
   print ('윤년 입니다.')
 else:
   print ('윤년이 아닙니다.')
+
+'''
+김영원입니다.
+year = int(input("년도를 입력하세요 : "))
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0 :
+    print("윤년")
+else: print("비윤년")
+'''
